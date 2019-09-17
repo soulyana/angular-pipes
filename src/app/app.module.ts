1c9638a3f19d9cde28dfda246b74406192c3f3b8
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-17 18:36:18 -0400

    creating a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ShortenPipe]
 })
 export class AppModule { }
