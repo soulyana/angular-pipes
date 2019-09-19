48782112c0f341b29cd4c3c6d8bf57b12186c400
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-19 12:01:04 -0400

    pure and impure pipes (or: how to "fix" the filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,7 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+  filteredStatus = '';
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
@@ -40,7 +41,12 @@ export class AppComponent {
     };
   }
 
-  filterStatus() {
-    
+  onAddServer() {
+     this.servers.push({
+       instanceType: 'small',
+       name: 'New Server',
+       status: 'stable',
+       started: new Date(15, 1, 2017)
+     });
   }
 }
